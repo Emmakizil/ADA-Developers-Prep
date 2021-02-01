eb3dce7c7b5a4ce689ef7ba4e6f54bf5618c9dab
FModabber <76422648+FModabber@users.noreply.github.com>
2021-01-31 23:46:58 -0800

    //added new function for multiply

---
+++ practice.js
@@ -0,0 +1,9 @@
+// function for division
+// use loop
+function divide(dividend, diviser){
+    let quotient = 0;
+    for (let i = dividend; i>0 ; i-=diviser){   
+     quotient++
+    } return quotient
+}
+console.log(divide(20,5));
