5b44f158afb934fee86ec4250d6e6dd81a49d154
FModabber <76422648+FModabber@users.noreply.github.com>
2021-02-03 10:25:01 -0800

    Added function for log using "for" loop

--- practice.js
+++ practice.js
@@ -7,3 +7,24 @@ function divide(dividend, diviser){
     } return quotient
 }
 console.log(divide(20,5));
+
+//function for multiplication, use "while loop"
+function multiply(multiplicand, multiplier){
+  let product = 0
+  for (let i = 0; i < multiplier; i++){
+    product += multiplicand
+  } return product
+}
+console.log(multiply(6,3))
+
+//function for exponents
+  function result(base, exponent) {
+    let result = 1;
+    for (i = 0; i < exponent; i++) {
+    result *= base;
+    }
+    return result;
+} 
+console.log(result(2,6))
+
+//function for logarithm
